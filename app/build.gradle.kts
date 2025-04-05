69a9c1a61b20e175ba6ef7e3d2717c1a0447b355
sja1383 <sja1383@wgu.edu>
2025-04-05 00:37:53 -0400

    - updated version in build.gradle.kts to update app bundle and update version on play console

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.dset.expensetracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
